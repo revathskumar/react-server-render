90ddbd7b5aad0c502e188cab0c1858bf24ff08af
Revath S Kumar <rsk@revathskumar.com>
2016-06-12 01:32:23 +0530

    Client App with Ajax request

--- client/app.jsx
+++ client/app.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {Router, browserHistory, Route} from 'react-router';
+
+class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {items: []};
+  }
+
+  componentDidMount() {
+    this.fetchList();
+  }
+
+  fetchList() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        this.setState({
+          items: data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.items.map(item => {
+          return <li key={item.id}>{item.name}</li>;
+        })}
+      </ul>
+    );
+  }
+}
 
 const App = () => {
   return (
     <div>
       <h2>React Universal App</h2>
+      <List/>
     </div>
   );
 };
